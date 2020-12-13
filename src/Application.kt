d1f77691e2222d4824a79b38365555b4ccb83c0a
Lubomir1998 <lubcho_sk8@abv.bg>
2020-12-14 00:06:46 +0200

    Added all dependencies and installed the needed features

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,32 @@
 package com.example
 
 import io.ktor.application.*
+import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.gson.gson
 import io.ktor.response.*
 import io.ktor.request.*
+import io.ktor.routing.Route
+import io.ktor.routing.Routing
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(ContentNegotiation) {
+        gson {
+            setPrettyPrinting()
+        }
+    }
+
+    install(Routing) {
+
+    }
+
 }
 
